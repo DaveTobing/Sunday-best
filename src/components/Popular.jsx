6581ad35879cd8b03f1a7ec55755d0afb7e49b22
Dave Matthew Peter Lumban Tobing <davetobing4@gmail.com>
2023-09-21 13:26:09 +0700

    Popular Page 80%

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Bottle from '../assets/bottle.svg'
 
 const Popular = () => {
   return (
-    <div className='flex justify-center h-4/5  bg-background-light-200'>
-        <div className='m-5'>
+    <div className='flex flex-col justify-center h-4/5 bg-background-light-200'>
+        <div className='m-10'>
             <h1 className='text-center font-bold text-4xl'>
                 Our Popular Booze
             </h1>
@@ -14,8 +15,89 @@ const Popular = () => {
                 spirits. All products 100% ORIGINAL
             </h2>
         </div>
-        <div>  
-                      
+
+        <div className='flex flex-row grid-cols-4 justify-around m-10'>  
+            <div className='flex flex-col bg-background-light-250 rounded-xl w-64'>
+                    <img src={Bottle} alt="" className='w-64' />
+                        <div className='bg-background-light-300 rounded-t-3xl z-[2]'>
+                            <p className='text-center'>
+                                    Gordon's Gin 
+                            </p>
+                            <p className='text-center'>
+                                    Premium Pink
+                            </p>
+                            <div className='underline bg-[#1D1D1D] h-0.5 w-1/2 mx-auto rounded-lg'></div>
+                            <div className='flex flex-row gap-32'>
+                                <p>
+                                    Rp299.000
+                                </p>
+                                <p>
+                                    700 ml
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            <div className='flex flex-col bg-background-light-250 rounded-xl w-64'>
+                    <img src={Bottle} alt="" className='w-64' />
+                        <div className='bg-background-light-300 rounded-t-3xl z-[2]'>  
+                            <p className='text-center'>
+                                Gordon's Gin 
+                            </p>
+                            <p className='text-center'>
+                                Premium Pink
+                            </p>
+                            <div className='underline bg-[#1D1D1D] h-0.5 w-1/2 mx-auto rounded-lg'></div>
+                            <div className='flex flex-row gap-32'>
+                                <p>
+                                    Rp299.000
+                                </p>
+                                <p>
+                                    700 ml
+                                </p>
+                        </div>
+                </div>
+            </div>
+            <div className='flex flex-col bg-background-light-250 rounded-xl w-64'>
+                <img src={Bottle} alt="" className='w-64' />
+                    <div className='bg-background-light-300 rounded-t-3xl z-[2]'>
+                    <p className='text-center'>
+                            Gordon's Gin 
+                    </p>
+                    <p className='text-center'>
+                            Premium Pink
+                    </p>
+                    <div className='underline bg-[#1D1D1D] h-0.5 w-1/2 mx-auto rounded-lg'></div>
+                    <div className='flex flex-row gap-32'>
+                        <p>
+                            Rp299.000
+                        </p>
+                        <p>
+                            700 ml
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-col bg-background-light-250 rounded-xl w-64'>
+                <img src={Bottle} alt="" className='w-64' />
+                    <div className='bg-background-light-300 rounded-t-3xl z-[2]'>
+                    <p className='text-center'>
+                            Gordon's Gin 
+                    </p>
+                    <p className='text-center'>
+                            Premium Pink
+                    </p>
+                    <div className='underline bg-[#1D1D1D] h-0.5 w-1/2 mx-auto rounded-lg'></div>
+                    <div className='flex flex-row gap-32'>
+                        <p>
+                            Rp299.000
+                        </p>
+                        <p>
+                            700 ml
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,51 +8,24 @@ module.exports = {
         'max-height': 'max-height'
       },
       fontFamily: {
-        sigika: ['Sigika', 'sans'], 
+        signika: ['Signika', 'sans'], 
       },
       colors: {
         light : {
           100: '#F5F5F5',
           200: '#F1F1F1',
+          250: '#FFFAF4',
           300: '#FEEB09',
           400: '#F1EA3E'
-        },
-        dark: {
-          100 : '#D9D9D9', 
-          200 : '#A5C9CA',
-          300 : '#395B64',
-          350 : '#416772',
-          400 : '#2C3333'
         },
         background: {
           light : {
             100: '#F5F5F5',
             200: '#F1F1F1',
+            250: '#FFFAF4',
             300: '#FEEB09',
             400: '#00A2EA'
           },
-          dark: {
-            100 : '#D9D9D9', 
-            200 : '#A5C9CA',
-            300 : '#395B64',
-            350 : '#416772',
-            400 : '#2C3333'
-          }
-        },
-        primary: {
-          100: '#CEEFDB',
-          200: '#9CCCAF',
-          300: '#7BA88D'
-        },
-        secondary: {
-          100: '#E3D7FF',
-          200: '#D1CEF8',
-          300: '#BFB3F1'
-        },
-        accent: {
-          100: '#FFC8C8',
-          200: '#FF7B7B',
-          300: '#FF2E2E'
         },
       },
     },
