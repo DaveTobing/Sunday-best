49f2d0b6280846f10e5c415e5d437fa4b5594179
Dave Matthew Peter Lumban Tobing <davetobing4@gmail.com>
2023-09-24 23:04:00 +0700

    changes in categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import Soju from '../assets/Soju.svg'
-import Sake from '../assets/Sake.svg'
-import Wine from '../assets/Wine.svg'
-import Whisky from '../assets/Whisky.svg'
-import Gin from '../assets/Gin.svg'
-import Vodka from '../assets/Vodka.svg'
-import Rum from '../assets/Rum.svg'
-import Tequila from '../assets/Tequila.svg'
-import Cognac from '../assets/Cognac.svg'
-import Beers from '../assets/Beers.svg'
-import Other from '../assets/Others.svg'
+import Soju from '../assets/Soju.png'
+import Sake from '../assets/Sake.png'
+import Wine from '../assets/Wine.png'
+import Whisky from '../assets/Whisky.png'
+import Gin from '../assets/Gin.png'
+import Vodka from '../assets/Vodka.png'
+import Rum from '../assets/Rum.png'
+import Tequila from '../assets/Tequila.png'
+import Cognac from '../assets/Cognac.png'
+import Beers from '../assets/Beers.png'
+import Other from '../assets/Others.png'
+import Brandy from '../assets/Brandy.png'
 
 const Categories = () => {
   return (
     <div className='mx-32 mt-16'>
-        <h1 className='font-bold font-signika text-3xl'>Categories</h1>
+        <h1 className='font-bold font-signika text-3xl text-center uppercase'>Categories</h1>
         <div className='flex flex-wrap gap-4 mt-5'>
             <img src={Soju} alt="" className='hover:scale-105 duration-300'/>
             <img src={Sake} alt="" className='hover:scale-105 duration-300'/>
             <img src={Wine} alt="" className='hover:scale-105 duration-300'/>
+            <img src={Brandy} alt="" className='hover:scale-105 duration-300'/>
             <img src={Whisky} alt="" className='hover:scale-105 duration-300'/>
             <img src={Gin} alt="" className='hover:scale-105 duration-300'/>
             <img src={Vodka} alt="" className='hover:scale-105 duration-300'/>
