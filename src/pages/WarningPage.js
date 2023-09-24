f0ae7b9142df7995ac8d4c2848c3838774c2fc9d
Joshua Mihai Daniel Nadeak <joshua.mihai@ui.ac.id>
2023-09-24 17:34:01 +0700

    fix: final for warningpage

--- src/pages/WarningPage.js
+++ src/pages/WarningPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import './WarningPage.css';
+import logo from '../assets/logo.png';
 
 function WarningPage({ onVerify }) {
     const handleYesClick = () => {
@@ -10,8 +11,8 @@ function WarningPage({ onVerify }) {
     return (
         <div className="warning-wrapper">
             <div className="warningPage">
-                <div className="content">
-                    <img src="../assets/logo.png" alt="Logo" className="logo"/>
+                <div className="content font-signika">
+                    <img src={logo} alt="Logo" className="logo"/>
                     <h1>Toast to your special day!</h1>
                     <p>We need to verify you're 21 before we let you browse and purchase from this website</p>
                     <div className="buttons">
@@ -19,8 +20,12 @@ function WarningPage({ onVerify }) {
                         <button>No, I am not</button>
                     </div>
                     <div className="socials">
-                        <SocialIcon network="instagram" bgColor="transparent" style={{ height: 25, width: 25 }}/>
-                        <SocialIcon network="whatsapp" bgColor="transparent" style={{ height: 25, width: 25 }}/>
+                        <a href="https://www.instagram.com/sundaybestid/" target="_blank" rel="noopener noreferrer">
+                            <SocialIcon network="instagram" bgColor="transparent" style={{ height: 25, width: 25 }}/>
+                        </a>
+                        <a href="https://api.whatsapp.com/send/?phone=62818688448&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+                            <SocialIcon network="whatsapp" bgColor="transparent" style={{ height: 25, width: 25 }}/>
+                        </a>
                     </div>
                 </div>
             </div>
